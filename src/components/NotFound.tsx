5a6e3723b52628ffb4d5f1a95bb99b36fb2419e8
Magnus Vahlström <Wahlstrommm@gmail.com>
2022-03-25 15:24:19 +0100

    First commit

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../index.css';
+
+function NotFound() {
+  return (
+    <div className='App-container'>Hittar inte det du söker.... Tryck här för att komma tillbaka</div>
+  )
+}
+
+export default NotFound
